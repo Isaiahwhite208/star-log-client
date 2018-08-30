8b44de9e813f25139128125d642d625256acbf01
Isaiah White <isaiahwhite208@gmail.com>
2018-08-30 13:30:34 -0600

    push

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  mounted() {
+    //blocks users not logged in
+    if (!this.$store.state.user._id) {
+      this.$router.push({ name: "login" });
+    }
+  }
+};
+</script>
